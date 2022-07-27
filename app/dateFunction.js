a6e53bdba04aac27a39d9a6d1487f27d89ee0d1a
Anton Cherkas <99758388+Cherkasant@users.noreply.github.com>
2022-07-27 23:56:44 +0300

    Delete app directory

--- app/dateFunction.js
+++
@@ -1,7 +0,0 @@
-export const getDate = () => {
-  var date = new Date(Date.now());
-  var dateString = new Date(date.getTime() - date.getTimezoneOffset() * 60000)
-    .toISOString()
-    .split('T')[0];
-  return dateString;
-};
