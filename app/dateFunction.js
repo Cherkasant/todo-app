844c30e0c793b7dc1bfe121ade308394a6ae4b64
Anton Cherkas <99758388+Cherkasant@users.noreply.github.com>
2022-07-29 18:27:55 +0300

    Update dateFunction.js

--- app/dateFunction.js
+++ app/dateFunction.js
@@ -1,6 +1,6 @@
 export const getDate = () => {
-  var date = new Date(Date.now());
-  var dateString = new Date(date.getTime() - date.getTimezoneOffset() * 60000)
+  let date = new Date(Date.now());
+  let dateString = new Date(date.getTime() - date.getTimezoneOffset() * 60000)
     .toISOString()
     .split('T')[0];
   return dateString;
